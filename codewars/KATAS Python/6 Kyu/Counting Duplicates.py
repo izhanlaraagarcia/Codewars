1a8a232bc3994da652a0890d233840dd54fb6cb5
Izhan Lara García <72951122+izhanlaraagarcia@users.noreply.github.com>
2023-02-15 17:21:51 +0100

    Counting Duplicates Python

---
+++ codewars/KATAS Python/6 Kyu/Counting Duplicates.py
@@ -0,0 +1,19 @@
+def duplicate_count(text):
+    text_lower = text.lower()
+    count = 0
+    dic = {}
+    
+    for i in range(0,len(text_lower)):
+        dic[text_lower[i]] = 0
+        
+    
+    for key in dic:
+        for i in range(0,len(text_lower)):
+            if(key == text_lower[i]):
+                dic[key] = dic[key] + 1
+            
+    for key in dic:
+        if (dic[key] > 1):
+            count = count + 1
+    
+    return count
