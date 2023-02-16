9f8e41e05818afd69a0f20f3584453eb67360fca
Izhan Lara García <72951122+izhanlaraagarcia@users.noreply.github.com>
2023-02-16 10:14:43 +0100

    Square(n) Sum Python

---
+++ codewars/KATAS Python/8 Kyu/Square(n) Sum.py
@@ -0,0 +1,5 @@
+# Square(n) Sum
+# This is the result of the "Square(n) Sum" 8 kyu exercise in Codewars, language used: Python
+# Codewars
+def square_sum(numbers):
+    return sum(i ** 2 for i in numbers)
