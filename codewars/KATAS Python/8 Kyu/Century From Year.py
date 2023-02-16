d0912c79ed54c42fa228dfa57e1bdcb4f89e86cb
Izhan Lara García <72951122+izhanlaraagarcia@users.noreply.github.com>
2023-02-16 14:24:29 +0100

    Century From Year

---
+++ codewars/KATAS Python/8 Kyu/Century From Year.py
@@ -0,0 +1,7 @@
+# Century From Year
+# This is the result of the "Century From Year" 8 kyu exercise in Codewars, language used: Python
+# Codewars
+
+def century(year):
+    
+    return (year-1) // 100 + 1
