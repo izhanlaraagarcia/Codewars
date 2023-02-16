4f720b3a4e3e3b7b22d540c7907b8fe4ac1b0003
Izhan Lara García <72951122+izhanlaraagarcia@users.noreply.github.com>
2023-02-16 14:35:42 +0100

    The 'if' function

---
+++ codewars/KATAS Python/8 Kyu/The 'if' function.py
@@ -0,0 +1,5 @@
+# The 'if' function
+# This is the result of the "The 'if' function" 8 kyu exercise in Codewars, language used: Python
+# Codewars
+def _if(bool, func1, func2):
+    return func1 () if bool else func2()
